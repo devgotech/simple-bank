85efdc364fcd480d19b67ec657f0148e35c1a088
dev.gotech <jamesgotech@gmail.com>
2024-09-30 03:12:54 +0100

    sorted most db deadlock

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbdriver = "postgres"
+	dbsource = "postgresql://root:7090@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+
+	testDB, err = sql.Open(dbdriver, dbsource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
